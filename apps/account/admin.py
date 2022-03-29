dba54a8e5ffcd585a0c144158995fd87cf7402ec
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-29 08:32:16 +0000

    event and user admin

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    search_fields = ("email", "firstname", "lastname", "middlename")
+    list_display = ("email", "firstname", "lastname", "middlename", "is_active", "is_staff")
+    ordering = ("-date_joined",)
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "email",
+                    "firstname",
+                    "lastname",
+                    "middlename",
+                    "date_of_birth",
+                    "profile_picture",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+        ("Groups", {"fields": ("groups",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "firstname",
+                    "lastname",
+                    "middlename",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- apps/event/admin.py
+++ apps/event/admin.py
@@ -1,50 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-
-
-class CustomUserAdmin(UserAdmin):
-    search_fields = ("email", "firstname", "lastname", "middlename")
-    list_display = ("email", "firstname", "lastname", "middlename", "is_active", "is_staff")
-    ordering = ("-date_joined",)
-    list_filter = ("is_staff", "is_superuser", "is_active")
-    fieldsets = (
-        (
-            "Profile",
-            {
-                "fields": (
-                    "email",
-                    "firstname",
-                    "lastname",
-                    "middlename",
-                    "date_of_birth",
-                    "profile_picture",
-                )
-            },
-        ),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
-        ("Important dates", {"fields": ("last_login",)}),
-        ("Groups", {"fields": ("groups",)}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": (
-                    "email",
-                    "firstname",
-                    "lastname",
-                    "middlename",
-                    "password1",
-                    "password2",
-                    "is_staff",
-                    "is_active",
-                ),
-            },
-        ),
-    )
+from .models import Event
 
 
-admin.site.register(User, CustomUserAdmin)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ("title", "organizer", "type", "category", "location", "venue")
+    list_display = ("title", "organizer", "type", "category", "location", "venue")
+    ordering = ("title",)
+
+
+
+admin.site.register(Event, EventAdmin)
