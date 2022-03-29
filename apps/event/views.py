c0e8f8709f164b1210455809aec26c2787712d14
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-29 14:07:20 +0000

    detail view

--- apps/event/views.py
+++ apps/event/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Event
 
 
 def event_detail(request, pk):
-    context = get_object_or_404(Event, pk=pk)
-    return render(request, "event/event_detail.html", context)
+    context = {"event": get_object_or_404(Event, pk=pk)}
+    return render(request, "event_detail.html", context)
